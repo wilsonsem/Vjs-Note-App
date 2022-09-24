a59a75e2acfa8988e7cfe8a4f015ab68d26a254b
Semmwa <bakshaksemmwa@gmail.com>
2022-09-24 13:44:43 +0100

    implemented modal funtionality and form validation

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
-let addBtn = document.querySelector("#add-btn")
-let noteTitle = document.querySelector("#title-field")
-let noteBody = document.querySelector("#text-field")
+let addBtn = document.querySelector("#add-btn"),
+    noteTitle = document.querySelector("#title-field"),
+    noteBody = document.querySelector("#text-field"),
+    modalText = document.querySelector(".modal-text")
+
+
+addBtn.addEventListener("click", () =>{
+    console.log("working")
+    if(noteTitle.value == ""){
+        modalText.innerHTML = "Add a Title";
+        modalText.classList.add("text-danger");
+    }
+    else if(noteBody.value == ""){
+        modalText.innerHTML = "Note cannot be empty";
+        modalText.classList.add("text-danger");
+    }
+    else if (noteTitle.value == "" || noteBody.value == ""){
+        modalText.innerHTML = "please fill out the fields";
+        modalText.classList.add("text-danger");
+    }
+    
+}) 
