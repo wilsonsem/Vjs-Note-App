5253de52e7200c008c296e1b8d46c6566078f02c
Semmwa <bakshaksemmwa@gmail.com>
2022-09-26 19:17:04 +0100

    fixed username prompt issue

--- js/main.js
+++ js/main.js
@@ -11,12 +11,18 @@ let addBtn = document.querySelector("#add-btn"),
 
 window.addEventListener("load", () =>{
 
-    let userName = localStorage.getItem('userName') || '';
-    // displayName.innerHTML = userName.toUpperCase();
-    userName = prompt('what should we call you');
+    let userName = localStorage.getItem('userName')
+    // let userName = localStorage.getItem('userName') || '';
+    // userName = prompt('what should we call you');
     // if(userName){
-        localStorage.setItem('userName' , userName)
+    //     localStorage.setItem('userName' , userName)
     // }
+    if(userName == "null"){
+        userName = prompt('what should we call you');
+        localStorage.setItem('userName' , userName)
+    }else {
+        userName = localStorage.getItem('userName');
+    }
     displayName.innerHTML = userName;
 
     if(noteTitle.value == ""){
@@ -92,27 +98,67 @@ window.addEventListener("load", () =>{
     }
     showNotes();
 
-    
-    // delete a note
-    function deleteNote(){
-        let confirmDelete = confirm("Are you sure you want to delete?");
-        if(confirmDelete == true){
-            let notes = localStorage.getItem("notes");
 
-            if(notes == null){
-                notesObj = [];
-            }else{
-                notesObj = JSON.parse(notes);
-            }
-        }
-        notesObj.splice(index, 1);
-        localStorage.setItem("notes", JSON.stringify(notesObj));
-        showNotes()
-    }
+   
     
 })
 
 
+  //function to display notes
+    function showNotes(){
+        let notes = localStorage.getItem("notes");
+
+        if(notes == null){
+            notesObj = [];
+        }else {
+            notesObj = JSON.parse(notes);
+        }
+
+        let html = "";
+        notesObj.forEach(function(element, index){
+            html += `
+            <div class="col-md-6 mt-2">
+                <div class="card">
+                    <div class="card-header fs-6 fw-bold text-uppercase">${element.title}</div>
+                    <div class="card-body">
+                        <p class="card-text">${element.body}</p>
+                    </div>
+                    <div class="text-end p-3">
+                        <button id="${index}" class="btn-success">EDIT</button>
+                        <button id="${index}" class="btn-danger" onclick="deleteNote(this.id)">DELETE</button>
+                    </div>
+                </div>
+            </div>`;
+    });
+
+    let allNotes = document.querySelector(".all-notes");
+    if(notesObj.length != 0){
+        allNotes.innerHTML = html;
+    }else{
+        noNotes.classList.add("show-text");
+    }
+}
+showNotes();
+
+
+
+ // delete a note
+function deleteNote(index){
+    console.log("deleted")
+    let confirmDelete = confirm("Are you sure you want to delete?");
+    if(confirmDelete == true){
+        let notes = localStorage.getItem("notes");
+
+        if(notes == null){
+            notesObj = [];
+        }else{
+            notesObj = JSON.parse(notes);
+        }
+    }
+    notesObj.splice(index, 1);
+    localStorage.setItem("notes", JSON.stringify(notesObj));
+    showNotes();
+}
     
 
 
